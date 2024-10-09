1e7c7a476c156a7b1f3f49326e3593f88feb5caf
Gabriel <gbrito341@gmail.com>
2024-10-09 15:55:03 -0300

    Eureka

--- apiGlpi/apiGlpi.py
+++ apiGlpi/apiGlpi.py
@@ -7,8 +7,6 @@ user_token = "L6an8rJwuy1bYjHwx72a6ytdLwz80B7aSWoNc1yW"
 app_token = "iXrMsWsxvJoF2ikQsPLFa8W8yFv9JZQnWxnBkjBG"
 auth = "user_token L6an8rJwuy1bYjHwx72a6ytdLwz80B7aSWoNc1yW"
 
-
-
 def initSession():
    headers = {
         "Authorization": f"{auth}",
@@ -44,11 +42,73 @@ def getMyProfiles(session_token):
    response = requests.request("GET", url, headers=headers)
    print(response.json())
 
+def getMyEntities(session_token):
+   headers = {
+      "Session-Token": f"{session_token}",
+      "App-Token": f"{app_token}",
+      "Content-Type": "application/json"
+   }
+
+   url = f"{baseUrl}/getMyEntities"
+
+   response = requests.request("GET", url, headers=headers)
+   print(response.json())
+
+def searchOptions(session_token):
+   headers = {
+      "Session-Token": f"{session_token}",
+      "App-Token": f"{app_token}",
+      "Content-Type": "application/json"
+   }
+
+   url = f"{baseUrl}/listSearchOptions/Ticket"
+
+   response = requests.request("GET", url, headers=headers)
+   print(response.json())
+
+def getMassiveActions(session_token):
+   headers = {
+      "Session-Token": f"{session_token}",
+      "App-Token": f"{app_token}",
+      "Content-Type": "application/json"
+   }
+
+   url = f"{baseUrl}/getMassiveActions/Ticket/"
+
+   response = requests.request("GET", url, headers=headers)
+   print(response.json())
+
+# curl -X PUT \
+# -H 'Content-Type: application/json' \
+# -H "Session-Token: YOUR_SESSION_TOKEN" \
+# -H "App-Token: YOUR_APP_TOKEN" \
+# -d '{"input": {"id": TICKET_ID, "status": 6, "solution": "Aprovado"}}' \
+# 'http://path/to/glpi/apirest.php/Ticket/TICKET_ID'
+
+def updateItem(session_token, ticket_id):
+   headers = {
+      "Session-Token": f"{session_token}",
+      "App-Token": f"{app_token}",
+      "Content-Type": "application/json"
+   }
+
+   url = f"{baseUrl}/Ticket/{ticket_id}"
+
+   payload = {
+      "input":{
+         "id":f"{ticket_id}",
+         "status":6,
+         "solution":"Aprovado"
+      }
+   }
+
+   response = requests.request("PUT", url, headers=headers, json=payload)
+   print(response.json())
 
 def main():
    session_token = initSession()
 
-   getMyProfiles(session_token)
+   updateItem(session_token)
 
    killSession(session_token)
 
