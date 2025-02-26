4c11cb6e5c264f43d80d76bd838beecc078af20e
Gabriel <gbrito341@gmail.com>
2025-02-26 16:50:10 -0300

    update main

--- scripts/screen_api_bot.bash
+++ scripts/screen_api_bot.bash
@@ -1,5 +1,5 @@
 #! /bin/bash
-screen -XS api_bot quit
-screen -dmS api_bot
-screen -S api-bot -p 0 -X stuff 'bash /home/bot/glpi/scripts/servidor_api_bot.bash
+screen -XS api_glpi quit
+screen -dmS api_glpi
+screen -S api_glpi -p 0 -X stuff 'bash /home/bot/glpi/scripts/servidor_api_bot.bash
 '
